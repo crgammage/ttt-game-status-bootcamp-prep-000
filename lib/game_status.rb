14b7d9e9aed63811879d4c5803e145a600dbba4b
masterful-array-3850 <crgammage@gmail.com>
2020-04-16 20:19:04 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -2,3 +2,11 @@
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
+
+WIN_COMBINATIONS = [
+  top_row_win = [0, 1, 2]
+  mid_row_win = [3, 4, 5]
+  bottom_row_win = [6, 7, 8]
+  [0, 4, 8]
+  [2, 4, 6]
+]
