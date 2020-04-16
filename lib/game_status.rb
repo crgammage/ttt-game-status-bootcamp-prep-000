5296a2db673ba9549befd8cc987df5f67d52f995
masterful-array-3850 <crgammage@gmail.com>
2020-04-16 21:22:22 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -47,9 +47,7 @@ def over?(board)
 end
 
 def winner(board)
-  if won?(board) == true
-    return board[won?(board)[0]]
-  else
-    return nil
-  end
+   if won?(board)
+      return board[won?(board)[0]]
+   end
 end
